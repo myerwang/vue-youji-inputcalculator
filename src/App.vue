96c34ef3153de4d761f43805f82acc97f2db5a1e
WANG MENGYU <myerwang@qq.com>
2021-12-22 17:05:48 +0900

    reset

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <input v-vyic>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vyic',
+  components: {}
+}
+</script>
+
+<style>
+#app {
+  width: 500px;
+  margin: 30vh auto;
+}
+</style>
